aacf8554fbbe4a69518cee1b68af8266b292f05b
Skyzary <132926053+Skyzary@users.noreply.github.com>
2025-12-16 23:50:26 +0100

    Add responsive burger menu: update header, mobile menu, and styles in HTML, CSS, and JS

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,11 +1,24 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuList = document.querySelector('.menu-items-list');
 const closeBtn = document.querySelector('.close-btn');
+const burgerBtn = document.querySelector('.burger-btn');
+
+burgerBtn.addEventListener('click', () => {
+  mobileMenu.classList.add('is-visible');
+  burgerBtn.classList.add('is-hidden');
+  closeBtn.classList.add('is-visible');
+});
+
 closeBtn.addEventListener('click', () => {
   mobileMenu.classList.remove('is-visible');
+  burgerBtn.classList.remove('is-hidden');
+  closeBtn.classList.remove('is-visible');
 });
+
 menuList.addEventListener('click', (target) => {
   if (target.target.nodeName === 'A') {
-    console.log(target.target);
     mobileMenu.classList.remove('is-visible');
-}})
+    burgerBtn.classList.remove('is-hidden');
+    closeBtn.classList.remove('is-visible');
+  }
+});
